663e6330c46a2f2f3917423d18f89c61e0a4d20c
Eduardo Alves <caaduh4@gmail.com>
2020-12-13 14:00:06 -0300

    Refactor: add password select

--- src/modules/Users/infra/mongoose/entities/User.js
+++ src/modules/Users/infra/mongoose/entities/User.js
@@ -6,7 +6,7 @@ const User = new Schema(
   {
     name: { type: String, required: true },
     email: { type: String, unique: true, lowercase: true },
-    password: { type: String, required: true },
+    password: { type: String, required: true, select: false },
     date_birth: { type: Date, required: true },
     phone: { type: Number, required: true },
     favorite_books: [{ type: Schema.Types.ObjectId, ref: 'Book' }],
