15c8b007085ed9b21489b8e2e35bc8ba499ccdfe
Eduardo Alves <caaduh4@gmail.com>
2020-12-13 13:58:40 -0300

    Refactor: auth in all routes user

--- src/modules/Users/infra/http/routes/users.routes.js
+++ src/modules/Users/infra/http/routes/users.routes.js
@@ -5,11 +5,13 @@ const favoriteBookController = require("../../../../Users/infra/http/controllers
 
 const Validate = require("../../../../../shared/middlewares/validate");
 const validatorsUser = require("../../../../../shared/validators/User");
-
 const Resolver = require('../../../../../shared/middlewares/adpter')
 
-usersRouter.get("/", Resolver(usersController.index))
+const ensureAuthenticad = require('../../../../Users/infra/http/middleware/ensureAuthenticad')
+
 usersRouter.post("/", Validate(validatorsUser.create), Resolver(usersController.create))
+usersRouter.use(ensureAuthenticad)
+usersRouter.get("/", Resolver(usersController.index))
 usersRouter.get("/:_id", Resolver(usersController.show))
 usersRouter.delete("/:_id", Resolver(usersController.remove))
 usersRouter.post("/:_id/favorite-book/:bookId", Resolver(favoriteBookController.favorite))
