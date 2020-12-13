8033647df007aee81a89bec4877823cdccc3de37
Eduardo Alves <caaduh4@gmail.com>
2020-12-13 01:52:17 -0300

    Config: config mongo and alter .env.example and package json

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -4,8 +4,6 @@ const mongoose = require('mongoose')
 const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_CONTAINER, MONGO_DB } = process.env
 
 const uri = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_CONTAINER}/${MONGO_DB}?authSource=admin`
-// const uri = 'mongodb+srv://root:2012289019@projects.kckpi.mongodb.net/library?retryWrites=true&w=majority'
-
 
 const config = {
   useNewUrlParser: true,
